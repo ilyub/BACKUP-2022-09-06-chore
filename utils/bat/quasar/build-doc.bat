ec7e4f5b7b78986d9a765e322a7d28711021bbc0
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-11-22 22:36:43 +0300

    feat(utils,quasar): Removed build-doc.bat

--- utils/bat/quasar/build-doc.bat
+++
@@ -1,8 +0,0 @@
-pushd .
-for %%p in ("%cd%") do set project=%%~np
-cd ../../../%project%
-php "%cd%/../chore/utils/php/clear-dir.php" docs
-php "%cd%/../chore/utils/php/vue-to-ts-init.php"
-call npm run build-doc
-php "%cd%/../chore/utils/php/vue-to-ts-done.php"
-popd
