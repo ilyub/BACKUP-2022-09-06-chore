928bd8107d08052c4eeaa000a4204f1df3a1bebc
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-11-22 23:42:11 +0300

    fix(utils): Fixed quasar-framework-npm.bat

--- utils/bat/typescript/install/dependencies-prod/quasar-framework-npm.bat
+++ utils/bat/typescript/install/dependencies-prod/quasar-framework-npm.bat
@@ -2,6 +2,6 @@ pushd .
 cd ..
 for %%p in ("%cd%") do set project=%%~np
 cd ../../../%project%
-call npm uninstall --no-audit @skylib/framework
-call npm install --no-audit --save-prod --strict-peer-deps @skylib/framework
+call npm uninstall --no-audit @skylib/quasar-framework
+call npm install --no-audit --save-prod --strict-peer-deps @skylib/quasar-framework
 popd
