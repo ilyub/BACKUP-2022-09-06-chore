529822a9704759384e02fa9c8fcf93eca723ac28
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-06 19:26:55 +0300

    feat(quasar-framework): Added "lodash" dependency

--- projects/quasar-framework/install/dependencies-peer.bat
+++ projects/quasar-framework/install/dependencies-peer.bat
@@ -1,7 +1,11 @@
 @echo off
 call "../../../utils/bat/typescript/install/%~n0" ^
   @quasar/quasar-app-extension-testing-unit-jest ^
+  @types/lodash ^
+  @types/lodash-es ^
   @types/lunr ^
+  lodash ^
+  lodash-es ^
   lunr ^
   vue
 if not "%1" == "nopause" pause
