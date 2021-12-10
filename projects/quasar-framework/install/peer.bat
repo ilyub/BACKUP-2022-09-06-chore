dc6dee2de08beb9e3e6dbc31fd6f42f2e4dd68c3
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-10 12:48:03 +0300

    feat(quasar-framework): Added dependencies

--- projects/quasar-framework/install/peer.bat
+++ projects/quasar-framework/install/peer.bat
@@ -1,9 +1,14 @@
 @echo off
 call "../../../utils/bat/typescript/install/%~n0" ^
+  @mdi/js-2@npm:@mdi/js ^
   @quasar/quasar-app-extension-testing-unit-jest ^
-  @types/natural-compare ^
+  @types/lodash ^
+  @types/lodash-es ^
   @types/lunr ^
-  natural-compare ^
+  @types/natural-compare ^
+  lodash ^
+  lodash-es ^
   lunr ^
+  natural-compare ^
   vue
 if not "%1" == "nopause" pause
