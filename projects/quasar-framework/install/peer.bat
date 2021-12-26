3ffc1bdfdb85d10930cb3792d4a52a540c8ae40e
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-27 02:13:28 +0300

    feat(quasar-framework): Added "vuedraggable@next" dependency

--- projects/quasar-framework/install/peer.bat
+++ projects/quasar-framework/install/peer.bat
@@ -1,11 +1,12 @@
 @echo off
 call "../../../utils/bat/typescript/install/%~n0" ^
-  @mdi/js-2@npm:@mdi/js ^
+  @mdi/js-dynamic@npm:@mdi/js ^
   @quasar/quasar-app-extension-testing-unit-jest ^
   @types/lodash ^
   @types/lodash-es ^
   @vue/test-utils ^
   lodash ^
   lodash-es ^
-  vue
+  vue ^
+  vuedraggable@next
 if not "%1" == "nopause" pause
