35af729575f3881527e6f82bee6f9d55c49d58a4
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-01-18 18:40:43 +0300

    feat(eslint-plugin): Added "@typescript-eslint/utils" dependency

--- projects/eslint-plugin/install/peer.bat
+++ projects/eslint-plugin/install/peer.bat
@@ -1,6 +1,6 @@
 @echo off
 call "../../../utils/bat/typescript/install/%~n0" ^
-  @typescript-eslint/experimental-utils ^
+  @typescript-eslint/utils ^
   tsutils ^
   typescript
 if not "%1" == "nopause" pause
