730696a0ab74fd8fe6cfc087e71d1b1a3ef74861
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-02-02 15:45:56 +0300

    feat(quasar-framework,time-manager): Edited .bat files

--- projects/quasar-framework/browserslist.bat
+++ projects/quasar-framework/browserslist.bat
@@ -1,3 +1,3 @@
 @echo off
-call "../../utils/bat/quasar/%~n0" %*
+call "../../utils/bat/typescript/%~n0" %*
 if not "%1" == "nopause" pause
--- utils/bat/quasar/tsc.bat
+++
@@ -1,7 +0,0 @@
-pushd .
-for %%p in ("%cd%") do set project=%%~np
-cd ../../../%project%
-php "%cd%/../chore/utils/php/vue-to-ts-init.php"
-call npm run tsc
-php "%cd%/../chore/utils/php/vue-to-ts-done.php"
-popd
