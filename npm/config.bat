482332757e30c4c40bbb527c6f6a8d6901faeffc
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-04-17 01:42:16 +0300

    feat(npm): Edited npm configuration

---
+++ npm/config.bat
@@ -0,0 +1,5 @@
+@echo off
+call npm config set init-author-name="Ilia Liubinskii"
+call npm config set init-author-email="admin.phpdev@gmail.com"
+call npm config set init-author-url="https://github.com/ilyub"
+pause
