8f6abd2f0ffefd40be97f13ab9e0bed0fc9a0e92
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-06-16 13:47:25 +0300

    feat(quasar-extension): Edited dependencies

--- projects/quasar-extension/install/peer.bat
+++ projects/quasar-extension/install/peer.bat
@@ -1,9 +1,11 @@
 @echo off
 call "../../../utils/bat/typescript/install/%~n0" ^
   @mdi/js-dynamic@npm:@mdi/js ^
+  @quasar/quasar-app-extension-testing ^
   @quasar/quasar-app-extension-testing-unit-jest ^
   @vue/test-utils ^
   maska ^
+  quasar ^
   type-fest ^
   vue ^
   vuedraggable@next
